81a80e776e0d31d51c43910de8cf830f38c86d98
xlecours <xavier.lecoursboucher@mcgill.ca>
2018-09-04 14:04:53 -0400

    copying from https://github.com/shots47s/cbrain-vagrant

---
+++ cbrain-vagrant/Vagrantfile
@@ -0,0 +1,58 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# All Vagrant configuration is done below. The "2" in Vagrant.configure
+# configures the configuration version (we support older styles for
+# backwards compatibility). Please don't change it unless you know what
+# you're doing.
+Vagrant.configure(2) do |config|
+  config.vm.box = "ubuntu/xenial64"
+
+  config.vm.network "forwarded_port", guest: 3000, host: 3000
+  # config.vm.network "public_network"
+  # config.vm.synced_folder "../data", "/vagrant_data"
+
+  required_plugins = %w( vagrant-vbguest vagrant-disksize )
+  _retry = false
+  required_plugins.each do |plugin|
+    unless Vagrant.has_plugin? plugin
+      system "vagrant plugin install #{plugin}"
+      _retry=true
+    end
+  end
+
+  if (_retry)
+      exec "vagrant " + ARGV.join(' ')
+  end 
+   
+  config.disksize.size="10GB"
+
+  config.vm.provider "virtualbox" do |vb|
+     vb.memory = "8129"
+  end
+
+  ## Set the timezone that you are in here
+  require 'time'
+  timezonetmp = (-1*((Time.zone_offset(Time.now.zone)/60)/60))
+  timezone = timezonetmp >- 1 ? "Etc/GMT+" + timezonetmp.to_s : "Etc/GMT" + timezonetmp.to_s 
+  config.vm.provision :shell, privileged: true, run: "always", inline: <<-SHELL
+      apt-get install ntp -y
+      sudo timedatectl set-timezone #{timezone}
+      sudo service ntp restart
+  SHELL
+  
+  # View the documentation for the provider you are using for more
+  # information on available options.
+
+  config.vm.provision "fix-no-tty", type: "shell" do |s|
+    s.privileged = false
+    s.path = "provision/setup.sh"
+  end
+
+  config.vm.provision "shell", privileged: false, run: "always", inline: <<-SHELL
+    cd ~/cbrain/BrainPortal
+    echo "Please login in to the portal for the first time as user 'admin'"
+    cat /tmp/cbinit.txt
+#    rails server thin -e development -p 3000 > ~/cbrain.log &
+  SHELL
+end
---
+++ cbrain-vagrant/provision/setup.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+
+logFile=/tmp/provision.log
+
+echo "Installing MySQL Server" > $logFile
+echo "-----------------------------------------------" >> $logFile
+sudo apt-get update
+sudo apt-get install mariadb-server -y >> $logFile
+
+sqlscript="/tmp/tmp.sql"
+
+echo "Creating MySQL Database" >> $logFile
+echo "-----------------------------------------------" >> $logFile
+
+touch $sqlscript
+echo "CREATE USER 'cbrain'@'localhost' IDENTIFIED BY 'CBRAIN';" >> $sqlscript
+echo "CREATE DATABASE cbrain_db CHARACTER SET 'utf8';" >> $sqlscript
+echo "GRANT ALL ON cbrain_db.* TO 'cbrain'@'localhost';" >> $sqlscript
+echo "FLUSH PRIVILEGES;" >> $sqlscript
+
+cat $sqlscript | sudo mysql -u root >> $logFile
+
+### Installing Ruby Version Manager
+
+echo "Installing Ruby Version Manager" >> $logFile
+echo "-----------------------------------------------" >> $logFile
+
+cd $HOME
+\curl -sSL https://rvm.io/mpapis.asc | gpg --import - >> $logFile
+\curl -sSL https://get.rvm.io | bash -s stable >> $logFile
+
+source $HOME/.rvm/scripts/rvm >> $logFile
+
+echo "Installing Ruby" > $logFile
+echo "-----------------------------------------------" >> $logFile
+
+rvm info >> $logFile
+rvm install 2.4 >> $logFile
+rvm --default 2.4 >> $logFile
+
+## Get CBRAIN Code
+
+echo "Cloning CBRAIN" >> $logFile
+echo "-----------------------------------------------" >> $logFile
+
+cd $HOME
+git clone https://github.com/aces/cbrain.git >> $logFile
+
+### Make sure we have all of the requirements
+
+echo "Installing additional packages" >> $logFile
+echo "-----------------------------------------------" >> $logFile
+
+sudo apt-get install libmysqlclient-dev -y >> $logFile
+sudo apt-get install libxml2 libxml2-dev -y >> $logFile
+sudo apt-get install libxslt1.1 -y >> $logFile
+
+## create database.yml file
+
+echo "Installing BrainPortal" >> $logFile
+
+cd $HOME/cbrain/BrainPortal/config
+sed 's/cbrain_db_name_here/cbrain_db/g' database.yml.TEMPLATE | sed 's/cbrain_db_user_name_here/cbrain/g' | sed 's/cbrain_db_user_pw_here/CBRAIN/g' > database.yml
+
+## install bundler
+
+gem install bundler >> $logFile
+cd $HOME/cbrain/BrainPortal; bundle install >> $logFile
+cd $HOME/cbrain/Bourreau; bundle install >> $logFile
+
+cd `bundle show sys-proctable`; rake install >> $logFile
+
+### For now, we will make this the developer setup
+
+cd $HOME/cbrain/BrainPortal/config/initializers
+sed 's/simple_name/CBRAINVagrant/g' config_portal.rb.TEMPLATE > config_portal.rb
+
+cd $HOME/cbrain/Bourreau/config/initializers
+sed 's/simple_name/LocalBourreau/g' config_bourreau.rb.TEMPLATE > config_bourreau.rb
+
+### Initialize Database Schema
+
+echo "Database Schema Initialization" >> $logFile
+cd $HOME/cbrain/BrainPortal
+rake db:schema:load RAILS_ENV=development >> $logFile
+rake db:seed RAILS_ENV=development | grep "admin password" > /tmp/cbinit.txt
+
+echo "Running Sanity Checks" >> $logFile
+rake db:sanity:check RAILS_ENV=development >> $logFile
+
+cd $HOME/cbrain/BrainPortal >> $logFile
+rake cbrain:plugins:install:all >> $logFile
+
+cd $HOME/cbrain/Bourreau >> $logFile
+rake cbrain:plugins:install:all >> $logFile
+
+### Make local directories for Caching and DP
+mkdir $HOME/BPCache
+mkdir $HOME/BorCache
+mkdir $HOME/BorWork
+mkdir $HOME/FlatLocalDP
+mkdir $HOME/SshDP
+mkdir $HOME/CBLocalDP
+
+### install docker 
+
+dockerLog=/tmp/docker-install.log
+sudo apt-get install docker.io -y > $dockerLog
+sudo usermod -a -G docker ubuntu >> $dockerLog
+
+## install singularity
+singLog=/tmp/singularity.log
+
+mkdir $HOME/singTemp; cd $HOME/singTemp
+sudo apt-get install python dh-autoreconf build-essential -y > $singLog 
+wget https://github.com/singularityware/singularity/releases/download/2.4.1/singularity-2.4.1.tar.gz >> $singLog
+tar -xvzf singularity-2.4.1.tar.gz >> $singLog
+cd singularity-2.4.1
+./configure --prefix=/usr/local --sysconfdir=/etc >> $singLog
+make >> $singLog
+sudo make install >> $singLog
+
+cd $HOME
+rm -rf $HOME/singTemp
+
+#### Set up the BrainPortal Cache and timezone
+cd $HOME/cbrain/BrainPortal
+
+echo $HOME | xargs -I {} echo 'p=RemoteResource.first; p.dp_cache_dir="{}/BPCache"; p.save' | rails c >> $logFile
+timezone=`timedatectl status | grep 'Time zone' | awk '{print $3}'`
+echo $timezone | xargs -I {} echo 'p=User.first; p.time_zone = "{}"; p.save' | rails c >> $logFile
+
+### Setup local DataProvider
+
+echo $HOME | xargs -I {} echo 'DataProvider.create :id => 2, :name => "LocalDP", :type => "FlatDirLocalDataProvider", :remote_dir => "{}/FlatLocalDP", :description => "Automatically Generated Local Data Provider", :user_id => 1, :group_id => 1, :online => 1' | rails c >> $logFile
+echo $timezone | xargs -I {} echo 'd=DataProvider.where("id=2").first; d.time_zone = "{}"; d.save' | rails c >> $logFile
+
+cat ~/.ssh/id_cbrain_portal.pub >> ~/.ssh/authorized_keys
+echo $HOME | xargs -I {} echo 'DataProvider.create :id => 3, :name => "SshDP", :type => "FlatDirSshDataProvider", :remote_dir => "{}/SshDP", :description => "Automatically Generated SSH Data Provider", :user_id => 1, :group_id => 1, :online => 1' | rails c >> $logFile
+echo $timezone | xargs -I {} echo 'd=DataProvider.where("id=3").first; d.time_zone = "{}"; d.save' | rails c >> $logFile
+echo $USER | xargs -I {} echo 'd=DataProvider.where("id=3").first; d.remote_user = "{}"; d.remote_host = "localhost"; d.remote_port = 22; d.save' | rails c >> $logFile
+
+echo $HOME | xargs -I {} echo 'DataProvider.create :id => 4, :name => "CbrainDP", :type => "EnCbrainSmartDataProvider", :remote_dir => "{}/CBLocalDP", :description => "Automatically Generated Smart Data Provider", :user_id => 1, :group_id => 1, :online => 1' | rails c >> $logFile
+echo $USER | xargs -I {} echo 'd=DataProvider.where("id=4").first; d.remote_user = "{}"; d.remote_host = "localhost"; d.remote_port = 22; d.save' | rails c >> $logFile
+echo $timezone | xargs -I {} echo 'd=DataProvider.where("id=4").first; d.time_zone = "{}"; d.save' | rails c >> $logFile
+
+#### Set up the local Bourreaux
+
+# Make sure you can see bundle at login
+cd $HOME
+echo 'export PATH="$PATH:$HOME/.rvm/bin"' > .tmpbashrc
+echo  '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> .tmpbashrc 
+cat .bashrc >> .tmpbashrc
+cp .bashrc .bashrc.old
+mv .tmpbashrc .bashrc
+
+# Create the Bourreau
+cd $HOME/cbrain/BrainPortal
+
+echo $HOME | xargs -I {} echo 'RemoteResource.create :id => 2, :name => "LocalBourreau", :type => "Bourreau", :user_id => 1, :group_id => 1, :online => 1, :description => "Automatically Generated Local Bourreau", :ssh_control_host => "localhost", :ssh_control_port => 22, :ssh_control_rails_dir => "{}/cbrain/Bourreau", :tunnel_mysql_port => 3333, :tunnel_actres_port => 3334, :dp_cache_dir => "{}/BorCache", :workers_instances=> 3, :workers_chk_time => 5, :workers_log_to => "combined", :workers_verbose => 1, :read_only => 0, :portal_locked => 0, :cache_trust_expire => 2592000, :cms_class => "ScirUnix", :cms_shared_dir => "{}/BorWork", :docker_executable_name => "docker", :singularity_executable_name => "singularity"' | rails c >> $logFile
+echo $USER | xargs -I {} echo 'p=RemoteResource.where("id=2").first; p.ssh_control_user = "{}"; p.save' | rails c >> $logFile
+echo $timezone | xargs -I {} echo 'p=RemoteResource.where("id=2").first; p.time_zone = "{}"; p.save' | rails c >> $logFile 
